b98d52f967409f9852d9b1061853277dca116e67
devsensei404 <subhadeeppaul51@gmail.com>
2026-07-02 02:28:46 +0000

    minor config changes and swapping hardcoded properties with env vars

--- backend/src/main/java/com/jobportal/MyConfig.java
+++ backend/src/main/java/com/jobportal/MyConfig.java
@@ -1,5 +1,9 @@
 package com.jobportal;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,5 +22,13 @@ public class MyConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
 }
